c44ba4401311281acdc0624dab1478fae575dae5
Hamza khan <hamzakhan127109@gmail.com>
2025-01-23 13:17:00 +0500

    lambda function (One line ftn)

---
+++ lambda.py
@@ -0,0 +1,16 @@
+double = lambda x: x*2
+print(double(5))    #output 10
+
+
+
+# find cube of a number
+cube = lambda x: x*x*x
+print(cube(3))   #output is 27
+
+
+
+# find average
+avg = lambda x,y,z: (x + y + z) / 3
+print(avg(3,9,10))   #out is 7.3333
+
+

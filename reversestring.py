65da5cd626d9870d881de272e448fc263ab0a67a
Hamza khan <hamzakhan127109@gmail.com>
2025-01-23 10:13:00 +0500

    create a function that reverse the string

---
+++ reversestring.py
@@ -0,0 +1,9 @@
+# Function to reverse a string
+def reverse_string(string):
+    return string[::-1]
+
+# Input
+word = input("Enter a string to reverse: ")
+
+# Output
+print(f"The reversed string is: {reverse_string(word)}.")

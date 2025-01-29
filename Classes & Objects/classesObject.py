d67b39d5594fe237554cce1867bfd1bb7f007001
Hamza khan <hamzakhan127109@gmail.com>
2025-01-29 12:16:46 +0500

    changes cls and obj

--- Classes & Objects/classesObject.py
+++ Classes & Objects/classesObject.py
@@ -1,15 +1,20 @@
 class Person:
-    name = "Hamza Khan"
-    occupation = "Student"
+    name = ""
+    occupation = ""
     def info(self):
         print(f"{self.name} is a {self.occupation}")
 
 
 a = Person()
+b = Person()
 a.name = "Muhammad"
 a.occupation = "Teacher"
+b.name = "Hammad khan"
+b.occupation = "Principal"
 a.info()
+b.info()
 
 #output
 
 # Muhammad is a Teacher
+# Hammaad khan is Principal

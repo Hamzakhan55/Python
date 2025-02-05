fa2f5c8155cb4d634b777f3767eac00a363fa389
Hamza khan <hamzakhan127109@gmail.com>
2025-02-05 21:41:44 +0500

    find a number large, small or equal to second number

---
+++ largestnum.py
@@ -0,0 +1,11 @@
+
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if (num1>num2):
+    print(f"{num1} is greater")
+elif (num1 == num2):
+    print(f"{num1} is equal to {num2}")
+else:
+    print(f"{num2} is greater")

97799c38272e13638cd6ea1a3e0e16e07fb0192b
Hamza khan <hamzakhan127109@gmail.com>
2025-02-24 12:11:49 +0500

    Factorial

--- factorial.py
+++ factorial.py
@@ -1,9 +1,11 @@
-# find factorial of a number
+num = int(input("Enter a number to find factorial: "))
 
 
-num = int(input("Enter a number: "))
-result = 1
-for i in range(2, num+1):
-    result = result*i
+if num == 0 or num == 1:
+    print(f"The factorial of {num} is 1")
+elif num > 1:
+    new_number = 1
+    for i in range(num, 0, -1):
+        new_number *= i
+    print(f"The factorial of {num} is {new_number}")
     
-print(f"The factorial of {num} is {result}")

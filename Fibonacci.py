14c54c5d407e22664f4ef8a394273d28cce8aed0
Hamza khan <hamzakhan127109@gmail.com>
2025-03-16 13:52:27 +0500

    Fibonacci series up to n terms

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,8 +1,10 @@
-def fibonacci(n):
-    if (n==0):
-        return 0
-    elif(n==1):
-        return 1
-    else:
-        return  fibonacci(n-1) + fibonacci(n-2)
-print(fibonacci(8))
+num = int(input("Enter a number to find fibonacci series: "))
+
+new_num = [0, 1]
+for i in range(2, num):
+    Last = new_num[-1]
+    second_last = new_num[-2]
+    next_num = Last + second_last
+    new_num.append(next_num)
+
+print(new_num)

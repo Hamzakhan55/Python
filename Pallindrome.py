557a3a974e6d2cad77a3497554a9d2b6169ecaf4
Hamza khan <hamzakhan127109@gmail.com>
2025-04-26 23:07:40 +0500

    Program to find the sum of digits of a number.

--- Pallindrome.py
+++ Pallindrome.py
@@ -11,3 +11,16 @@ if new_word == new_str:
     print(f"your string {word} is pallindrome")
 else:
     print(f"Your string {word} is not Pallindrome")
+
+
+# number = int(input("Enter a string to check pallindrome : "))
+
+# new_number = 0
+# index = len(number) - 1
+# while number >= 0:
+#     new_number += number[index]
+#     number -= 1
+# if number == new_number:
+#     print(f"Your number {number} is Pallindrome")
+# else:
+#     print(f"Your number {number} is not pallindrome")
---
+++ sum_digits_numbers.py
@@ -0,0 +1,10 @@
+numbers = input("Enter a number: ")
+sum_num = 0
+for i in numbers:
+    sum_num += int(i)
+
+print(sum_num)
+
+
+# input 987
+# output 24
